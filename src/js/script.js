d6d47e62b0b436e2ad612bafb28a855bba81b654
eshulga <ewgenij.shulga@gmail.com>
2018-02-18 23:42:14 +0200

    add moviedb library

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
-alert("это тест")
+
+const successGetMoview = function(res){
+    const result = JSON.parse(res);
+    console.log('get movie list on search');
+    console.log(result);
+    console.log('////////////////////')
+}
+
+const errorGetMovie = function(){
+    console.log(arguments);
+}
+
+const successGetUpcomming = function(res){
+    const result = JSON.parse(res);
+    console.log('get latest movie');
+    console.log(result);
+    console.log('////////////////////')
+}
+
+const errorGetUpcomming = function(){
+    console.log(arguments);
+}
+
+theMovieDb.search.getMovie({"query":"Бойцовский%20клуб", "language":"ru-RUS"}, successGetMoview, errorGetMovie)
+
+
+
+theMovieDb.movies.getUpcoming({"language":"ru-RUS"}, successGetUpcomming, errorGetUpcomming)
+
+
