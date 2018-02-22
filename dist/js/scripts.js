f7b2a17cc4a02d2849e04c97fb4f06425c0c3eb2
oneofblaze <oneofblaze@gmail.com>
2018-02-22 22:21:32 +0200

    Fist temp commit.

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,21 @@
-"use strict";
+'use strict';
 
-alert("это тест");
+var sortByNameBtnUp = document.getElementById('sortByNameUp');
+var sortByNameBtnDown = document.getElementById('sortByNameDown');
+var sortByDateBtnUp = document.getElementById('sortByDateUp');
+var sortByDateBtnDown = document.getElementById('sortByDateDown');
+
+function sortingByNameUp() {
+  // как сортируем пока непонятно, что сюда заходит
+}
+
+function sortingByNameDown() {}
+
+function sortingByDateUp() {}
+
+function sortingByDateDown() {}
+
+sortByNameBtnUp.addEventListener('click', sortingByNameUp); //вешаем датчик "слушателя" клика на стрелочки
+sortByNameBtnDown.addEventListener('click', sortingByNameDown);
+sortByDateBtnUp.addEventListener('click', sortingByDateUp);
+sortByDateBtnDown.addEventListener('click', sortingByDateDown);
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-alert("это тест")
+var sortByNameBtnUp = document.getElementById('sortByNameUp');
+var sortByNameBtnDown = document.getElementById('sortByNameDown');
+var sortByDateBtnUp = document.getElementById('sortByDateUp');
+var sortByDateBtnDown = document.getElementById('sortByDateDown');
+
+function sortingByNameUp() {
+// как сортируем пока непонятно, что сюда заходит
+}
+
+function sortingByNameDown() {
+}
+
+function sortingByDateUp() {
+}
+
+function sortingByDateDown() {
+}
+
+sortByNameBtnUp.addEventListener('click', sortingByNameUp); //вешаем датчик "слушателя" клика на стрелочки
+sortByNameBtnDown.addEventListener('click', sortingByNameDown);
+sortByDateBtnUp.addEventListener('click', sortingByDateUp);
+sortByDateBtnDown.addEventListener('click', sortingByDateDown);
