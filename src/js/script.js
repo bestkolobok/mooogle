22984033d7839334db55281f14f266fc1eed9589
Yaroslav <porty41@gmail.com>
2018-02-27 20:51:41 +0200

    completed sort part

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,74 @@
-var sortByNameBtnUp = document.getElementById('sortByNameUp');
-var sortByNameBtnDown = document.getElementById('sortByNameDown');
-var sortByDateBtnUp = document.getElementById('sortByDateUp');
-var sortByDateBtnDown = document.getElementById('sortByDateDown');
+/*jshint esversion: 6 */
 
-function sortingByNameUp() {
-// как сортируем пока непонятно, что сюда заходит
-}
 
-function sortingByNameDown() {
-}
+//находтим и подготавливаем шаблон карточки фильма для дальнейшей работы
+const card = document.getElementById('movie-card').textContent.trim();
 
-function sortingByDateUp() {
-}
+//компилируем наш шаблон в метод с помощью Lodash для дальгейшего использования, где либо
+const compiledCard = _.template(card);
 
-function sortingByDateDown() {
-}
+//Находим место, куда мы будет вставлять карточки фильмов.
+const colectionWrapper = document.getElementById('searchMovie');
 
-sortByNameBtnUp.addEventListener('click', sortingByNameUp); //вешаем датчик "слушателя" клика на стрелочки
-sortByNameBtnDown.addEventListener('click', sortingByNameDown);
-sortByDateBtnUp.addEventListener('click', sortingByDateUp);
-sortByDateBtnDown.addEventListener('click', sortingByDateDown);
+// метод, который будет выполнен в случае удачного обращения к API MovieDB
+var successGetUpcomming = function successGetUpcomming(res) {
+
+    // парсим JSON в объект
+    var data = JSON.parse(res);
+
+
+    // выводим его в консоль что бы было наглядно
+    console.log('get movie list on search');
+    console.log(data);
+    console.log('////////////////////');
+
+    var sortByName = document.getElementById("byName");
+
+    let compare = function(a, b) {
+        if (a.title < b.title)
+            return -1;
+        if (a.title > b.title)
+            return 1;
+        return 0;
+    }
+
+    data.results.sort(compare)
+
+    sortByName.onclick = compare;
+
+    
+
+    //  function compared() {
+    //     data.results.sort(compare)
+    // }
+        
+
+
+
+
+    //проходимся по коллекции фильмов из ответа и обьект каждого из фильмов 
+    //передаем в ранее "скомпилированный" метод
+    data.results.forEach(item => {
+        console.log(item);
+        colectionWrapper.insertAdjacentHTML('beforeend', compiledCard({
+            item
+        }));
+    });
+
+    
+    
+};
+
+// Метод, который будет вызван в случае ошибки при обращении к API MovieDB 
+
+var errorGetUpcomming = function errorGetUpcomming() {
+    console.log(arguments);
+};
+
+
+//обращение к методу библиотеки для получения списка предстоящих премьер
+//данный метод приведен в качестве примера использования шаблона карточки фильма.
+//За более детальной информацией обратитесь к документации библиотеки
+theMovieDb.movies.getUpcoming({
+    "language": "ru-RUS"
+}, successGetUpcomming, errorGetUpcomming);
