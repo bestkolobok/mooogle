e306f60935509c9cc8bde3c62b56bfbfba16dcd4
olko0416 <Gala&050>
2018-03-02 18:00:53 +0200

    some logic

--- src/js/search_start.js
+++ src/js/search_start.js
@@ -1,18 +1,21 @@
 let movie_collection = document.getElementById('searchMovie');
 let search_blcok = document.getElementsByClassName('search');
+
 //место, куда пользователь вводит запрос
 let searchInput_onFocus = function (){
     document.getElementById('search-form_input_search').style.border = 'none';
-}
+};
 
 const onClick = (event) => {
+    //TODO put future button className in first if
     if (event.target.className === "Some Future Class") {
         search_blcok.classList.remove('search_hidden');
-        movie_collection.classList.add('bg');
+        movie_collection.classList.add('black-background');
     }
     if (event.target.className !== "search") {
         search_blcok.classList.add('search_hidden');
+        movie_collection.classList.remove('black-background');
     }
-}
+};
 
 document.addEventListener("click", onClick);
