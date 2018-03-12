7283559a71d90c77a05cfe5b5ec276a62c5b737e
Evgenii Shulga <ewgenij.shulga@gmail.com>
2018-03-12 22:13:02 +0200

    home page refactoring example

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1941,63 +1941,80 @@ window.addEventListener("load", function(){
 				document.querySelector(".head-1").style.display = "flex";
     }, false)
 }, false)
-/*jshint esversion: 6 */
+// /*jshint esversion: 6 */
 
-const colectionWrapper = document.getElementById('searchMovie');
-if(window.location.pathname == "/"){
+// const colectionWrapper = document.getElementById('searchMovie');
+// if(window.location.pathname == "/"){
 
 
-//находтим и подготавливаем шаблон карточки фильма для дальнейшей работы
-const premieresFilm = document.getElementById('premieresFilm').textContent.trim();
-const screenFilm = document.getElementById('screenFilm').textContent.trim();
-const top100Film = document.getElementById('top100Film').textContent.trim();
-const premieresSeries = document.getElementById('premieresSeries').textContent.trim();
-const screenSeries = document.getElementById('screenSeries').textContent.trim();
-const top100Series = document.getElementById('top100Series').textContent.trim();
-const compiledPremieresFilm = _.template(premieresFilm);
-const compiledScreenFilm = _.template(screenFilm);
-const compiledTop100Film = _.template(top100Film);
-const compiledPremieresSeries = _.template(premieresSeries);
-const compiledScreenSeries = _.template(screenSeries);
-const compiledTop100Series = _.template(top100Series);
+// //находтим и подготавливаем шаблон карточки фильма для дальнейшей работы
+// const premieresFilm = document.getElementById('premieresFilm').textContent.trim();
+// const screenFilm = document.getElementById('screenFilm').textContent.trim();
+// const top100Film = document.getElementById('top100Film').textContent.trim();
+// const premieresSeries = document.getElementById('premieresSeries').textContent.trim();
+// const screenSeries = document.getElementById('screenSeries').textContent.trim();
+// const top100Series = document.getElementById('top100Series').textContent.trim();
+// const compiledPremieresFilm = _.template(premieresFilm);
+// const compiledScreenFilm = _.template(screenFilm);
+// const compiledTop100Film = _.template(top100Film);
+// const compiledPremieresSeries = _.template(premieresSeries);
+// const compiledScreenSeries = _.template(screenSeries);
+// const compiledTop100Series = _.template(top100Series);
 
-var successGetUpcomming = function (res) {
-    var data = JSON.parse(res);
-        console.log(data);
-    colectionWrapper.innerHTML += compiledPremieresFilm({data})
-};
-var successgetNowPlaying = function (res) {
-    var data = JSON.parse(res);
-    colectionWrapper.innerHTML += compiledScreenFilm({data})
-};
-var successgetTopRated = function (res) {
-    var data = JSON.parse(res);
-    colectionWrapper.innerHTML += compiledTop100Film({data})
-};
-var successgetOnTheAir = function (res) {
-    var data = JSON.parse(res);
-    colectionWrapper.innerHTML += compiledPremieresSeries({data})
-};
-var successgetAiringToday = function (res) {
-    var data = JSON.parse(res);
-    colectionWrapper.innerHTML += compiledScreenSeries({data})
-};
-var successgetTopRated = function (res) {
-    var data = JSON.parse(res);
-    colectionWrapper.innerHTML += compiledTop100Series({data})
-};
+// var successGetUpcomming = function (res) {
+//     var data = JSON.parse(res);
+//         console.log(data);
+//     colectionWrapper.innerHTML += compiledPremieresFilm({data})
+// };
+// var successgetNowPlaying = function (res) {
+//     var data = JSON.parse(res);
+//     colectionWrapper.innerHTML += compiledScreenFilm({data})
+// };
+// var successgetTopRated = function (res) {
+//     var data = JSON.parse(res);
+//     colectionWrapper.innerHTML += compiledTop100Film({data})
+// };
+// var successgetOnTheAir = function (res) {
+//     var data = JSON.parse(res);
+//     colectionWrapper.innerHTML += compiledPremieresSeries({data})
+// };
+// var successgetAiringToday = function (res) {
+//     var data = JSON.parse(res);
+//     colectionWrapper.innerHTML += compiledScreenSeries({data})
+// };
+// var successgetTopRated = function (res) {
+//     var data = JSON.parse(res);
+//     colectionWrapper.innerHTML += compiledTop100Series({data})
+// };
 var error = function () {
     console.log(arguments);
 };
 
-theMovieDb.movies.getUpcoming({ "language": "ru-RUS"  }, successGetUpcomming, error);
-theMovieDb.movies.getNowPlaying({ "language": "ru-RUS"  }, successgetNowPlaying, error);
-theMovieDb.movies.getTopRated({ "language": "ru-RUS"  }, successgetTopRated, error);
-theMovieDb.tv.getOnTheAir({ "language": "ru-RUS"  }, successgetOnTheAir, error);
-theMovieDb.tv.getAiringToday({ "language": "ru-RUS"  }, successgetAiringToday, error);
-theMovieDb.tv.getTopRated({ "language": "ru-RUS"  }, successgetTopRated, error);
+theMovieDb.movies.getUpcoming({ "language": "ru-RUS"  },  successMovies, error);
+// theMovieDb.movies.getNowPlaying({ "language": "ru-RUS"  }, successgetNowPlaying, error);
+// theMovieDb.movies.getTopRated({ "language": "ru-RUS"  }, successgetTopRated, error);
+// theMovieDb.tv.getOnTheAir({ "language": "ru-RUS"  }, successgetOnTheAir, error);
+// theMovieDb.tv.getAiringToday({ "language": "ru-RUS"  }, successgetAiringToday, error);
+// theMovieDb.tv.getTopRated({ "language": "ru-RUS"  }, successgetTopRated, error);
 
+const upcommingWrapper = document.getElementById('upcoming_movies');
+
+function prepareResult (res, count) {
+
+    const data = JSON.parse(res);
+    return data.results.splice(0, count);
 }
+
+function successMovies (res){
+
+    const toShow = prepareResult(res, 6);
+  
+    toShow.forEach((item, i) => {
+        upcommingWrapper.insertAdjacentHTML('beforeend', compiledCard({item}));
+    });
+}
+
+
 if(window.location.pathname == '/movie.html'){
 
 var img = document.querySelector('.main_img');
