c7e04eff667a75b3fdda06bc43788f87c2d9d806
olko0416 <Gala&050>
2018-03-15 00:18:03 +0200

    some logic

--- src/js/search_start.js
+++ src/js/search_start.js
@@ -1,20 +1,25 @@
-let movie_collection = document.getElementById('black_background_wrapper');
+let movie_collection = document.getElementById('black_background');
 let search_blcok = document.getElementById('search');
 
 //место, куда пользователь вводит запрос
-let searchInput_onFocus = function (){
+let searchInput_onFocus = function () {
     document.getElementById('search-form_input_search').style.border = 'none';
 };
 
 const onClick = (event) => {
-    if (event.target.className === "head-1__search" || event.target.className === "head-1__input-search" || event.target.className === "head__search" ) {
+    debugger
+    if (event.target.className === "head-1__search" || event.target.className === "head-1__input-search"
+        || event.target.className === "head__search" || event.target.classList.contains('search-form_input_search')) {
+        debugger
         search_blcok.classList.remove('search_hidden');
         search_blcok.classList.add('search_show');
-        movie_collection.classList.add('black-background');
-    } else {
+        movie_collection.classList.add('black_background');
+    } else if (search_blcok.classList.contains('search_show') && !event.target.classList.contains('search')
+        && !event.target.classList.contains('logo') && event.target.nodeName !== 'INPUT') {
+        debugger
         search_blcok.classList.add('search_hidden');
         search_blcok.classList.remove('search_show');
-        movie_collection.classList.remove('black-background');
+        movie_collection.classList.remove('black_background');
     }
 };
 
