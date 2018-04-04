70a0cc74e297041077c7a0ef176c63c0a5e850c3
Evgenii Shulga <ewgenij.shulga@gmail.com>
2018-04-04 18:35:35 +0300

    update save favorites

--- src/js/favorits.js
+++ src/js/favorits.js
@@ -1,8 +1,8 @@
 
 
 (function checkStorage () {
-    if(localStorage.Mooogle === undefined){
-        localStorage.Mooogle = JSON.stringify([]);
+    if(localStorage.favorites === undefined){
+        localStorage.setItem("favorites", JSON.stringify([]));
     }
 })();
 
@@ -20,16 +20,61 @@ const favorites = {
     },
 
     saveStore: function() {
-        const mooogleStore = JSON.parse(localStorage.Mooogle);
-        mooogleStore.favorites = this.store;
-        localStorage.Mooogle = JSON.stringify(mooogleStore);
+
+        console.log('this store', this.store);
+
+        
+        const mooogleStore = {
+            favorites: JSON.parse(localStorage.favorites)
+        }
+        
+
+        console.log(this.store);
+
+        
+
+        mooogleStore.favorites[this.store[0].id] = this.store[0]
+        console.log(mooogleStore.favorites);
+        this.store = [];
+        localStorage.setItem("favorites", JSON.stringify(mooogleStore.favorites));
+    },
+
+    successGetInfo: function(result) {
+        let data = JSON.parse(result);
+
+        console.log(data);
+        
+        if(Object.getOwnPropertyNames(data).includes('first_air_date'))
+            data.release_date = data.first_air_date;
+        if(Object.getOwnPropertyNames(data).includes('name'))
+            data.title = data.name;
+
+        let dataToSave = {
+            title: data.title,
+            release_date: data.release_date,
+            id: data.id,
+            genres: data.genres
+        }
+
+        favorites.store.push(dataToSave);
+
+        favorites.saveStore();
+        
+    },
+
+    errorGetInfo: function(result) {
+        console.log(result);
     },
 
     add: function(id) {
         
-        this.store = [...this.store, ...[id]];
-        console.log(this)
-        this.saveStore();
+        if(id.length > 5){
+//movie     
+            theMovieDb.movies.getById({"id":id, "language": "ru-RUS",}, this.successGetInfo, this.errorGetInfo);
+        } else {
+//tvshow
+            theMovieDb.tv.getById({"id":id, "language": "ru-RUS",}, this.successGetInfo, this.errorGetInfo);
+        }
     },
 
     getAll: function() {
